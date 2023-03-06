790320ae7b264f63f1b08cbc534dea78c5b0aa29
dharvik120 <127028346+dharvik120@users.noreply.github.com>
2023-03-06 10:55:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,7 @@ export default function Home() {
       <Head>
         <title>CITY MART</title>
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"></link>
-        <meta name="description" content="GKredict is a decentralized launchpad that
-         allows users to launch their token and create their initial 
-         token sale with staking benefits to their holders and they don't 
-         require any Coding Knowledge For this." />
+        <meta name="description" content="CITY MART is a decentralized launchpad that allows users to launch their token and create their initial token sale with staking benefits to their holders and they don't require any Coding Knowledge For this." />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
